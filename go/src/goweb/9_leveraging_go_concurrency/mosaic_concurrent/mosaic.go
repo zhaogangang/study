7814a59c360c786070f22bee09a21f85c8f0313e
centos7 <739640713@qq.com>
2020-07-02 20:15:22 +0800

    add go mosaic

--- go/src/goweb/9_leveraging_go_concurrency/mosaic_concurrent/mosaic.go
+++ go/src/goweb/9_leveraging_go_concurrency/mosaic_concurrent/mosaic.go
@@ -1,5 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"image"
+	"image/color"
+)
 
-func 
+func averageColor(img image.Image) [3]float64 {
+	bounds := img.Bounds()
+	r, g, b := 0.0, 0.0, 0.0
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			r1, g1, b1, _ := img.At(x, y).RGBA()
+			r, g, b = r+float64(r1), g+float64(g1), b+float64(b1)
+		}
+	}
+	totalPixels := float64(bounds.Max.X * bounds.Max.Y)
+	return [3]float64{r / totalPixels, g / totalPixels, b / totalPixels}
+}
+
+func resize(in image.Image, newWidth int) image.NRGBA {
+	bounds := in.Bounds()
+	width := bounds.Dx()
+	ratio := width / newWidth
+	out := image.NewNRGBA(image.Rect(bounds.Min.X/ratio, bounds.Min.X/ratio, bounds.Max.X/ratio, bounds.Max.Y/ratio))
+	for y, j := bounds.Min.Y, bounds.Min.Y; y < bounds.Max.Y; y, j = y+ratio, j+1 {
+		for x, i := bounds.Min.X, bounds.Min.X; x < bounds.Max.X; x, i = x+ratio, i+1 {
+			r, g, b, a := in.At(x, y).RGBA()
+			out.SetNRGBA(i, j, color.NRGBA{uint8(r >> 8), uint8(g >> 8), uint8(b >> 8), uint8(a >> 8)})
+		}
+	}
+	return *out
+}
