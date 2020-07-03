5611fc3befb4bc995bedb9f724b4b397f390c57e
centos7 <739640713@qq.com>
2020-07-03 19:54:11 +0800

    add go chapter 9

--- go/src/goweb/9_leveraging_go_concurrency/mosaic_concurrent/mosaic.go
+++ go/src/goweb/9_leveraging_go_concurrency/mosaic_concurrent/mosaic.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"image"
 	"image/color"
+	"io/ioutil"
+	"math"
+	"os"
+	"sync"
 )
+
+type DB struct {
+	mutex *sync.Mutex
+	store map[string][3]float64
+}
 
 func averageColor(img image.Image) [3]float64 {
 	bounds := img.Bounds()
@@ -30,4 +40,60 @@ func resize(in image.Image, newWidth int) image.NRGBA {
 		}
 	}
 	return *out
+}
+
+func tilesDB() DB {
+	fmt.Println("Start populating tiles db ...")
+	var db DB
+	files, _ := ioutil.ReadDir("tiles")
+	for _, f := range files {
+		name := "tiles/" + f.Name()
+		file, err := os.Open(name)
+		if err == nil {
+			img, _, err := image.Decode(file)
+			if err == nil {
+				db.store[name] = averageColor(img)
+			} else {
+				fmt.Println("error in populating TILEDBS:", err, name)
+			}
+		} else {
+			fmt.Println("cannot open file", name, err)
+		}
+		file.Close()
+	}
+	fmt.Println("Finished populating tiles db.")
+	return db
+}
+
+func (db *DB) nearest(target [3]float64) string {
+	var filename string
+	db.mutex.Lock()
+	smallest := 1000000.0
+	for k, v := range db.store {
+		dist := distance(target, v)
+		if dist < smallest {
+			filename, smallest = k, dist
+		}
+	}
+	delete(db.store, filename)
+	db.mutex.Unlock()
+	return filename
+}
+
+func distance(p1 [3]float64, p2 [3]float64) float64 {
+	return math.Sqrt(sq(p2[0]-p1[0]) + sq(p2[1]-p1[1]) + sq(p2[2]-p1[2]))
+}
+
+func sq(n float64) float64 {
+	return n * n
+}
+
+var TILESDB DB
+
+func cloneTilesDB() DB {
+	var db DB
+	for k, v := range TILESDB.store {
+		db.store[k] = v
+	}
+	return db
 }
